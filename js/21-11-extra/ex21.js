b39a6853c3ac9aa83e9657df6dc36f74e1461f93
hothaifa zoubi <hothaifazoubi@gmail.com>
2022-11-21 10:43:46 +0200

    add extra

---
+++ js/21-11-extra/ex21.js
@@ -0,0 +1,45 @@
+//1
+// let circle = {
+//     PI:3.14159,
+//     CalcArea(radius=1){
+//         return radius * radius * this.PI;
+//     },
+//     CalcHekef(radius){ 
+//         return radius * 2 * this.PI;
+//     }
+// };
+
+
+// let dog = {
+//     name:'jojo the dog',
+//     kind: 'poodle',
+//     age: 11,
+//     favFood: 'bunzo',
+//     bark(){
+//         console.log(this.name + 'rouph rouph')
+//     }
+// };
+
+//3 
+// switch case is an other way to use if else-if
+// we only got to check if the value 
+// equals another value
+
+//4 
+// let date = new Date(2021,5,15);
+// console.log(date)
+
+// //5 
+// let newYearEve = new Date(2022,11,31);
+// // newYearEve.setMonth(2);
+// switch(newYearEve.getDay()){
+//     case 0: console.log('sun'); break;
+//     case 1: console.log('mon'); break;
+//     case 2,4: console.log('th'); break;
+//     case 3: console.log('wed'); break;
+//     case 5: console.log('fri'); break;
+//     case 6: console.log('sat'); break;
+// }
+
+
+
