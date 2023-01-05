7cd7714ce9055bd48f5910bf8ff202b3618bef30
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-01-05 16:34:51 +0200

    message

---
+++ exp1/routes/auth.js
@@ -0,0 +1,31 @@
+const _=require('lodash')
+const bcrypt =require('bcrypt')
+const express = require('express')
+const mongoose = require('mongoose')
+const Joi= require('joi')
+const {User} = require('../Models/user')
+const router = express.Router();
+
+
+router.post('/', async (req, res) => {
+  const {error} = validate(req.body)
+  if(error) return res.status(400).send(error.details[0].message)
+  
+  let user = await User.findOne({email:req.body.email})
+  if(!user) return res.status(400).send('Invalid email or password.')
+
+  const valiedPassword= await bcrypt.compare(req.body.password , user.password)
+  res.send(valiedPassword);
+})
+
+
+function validate(req) {
+    const schema = {
+        email: Joi.string().min(4).max(255).required().email(),
+        password: Joi.string().min(5).max(255).required()
+    };
+
+    return Joi.validate(req,schema)
+}
+
+module.exports = router
