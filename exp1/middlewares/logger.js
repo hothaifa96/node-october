3bcb9ef4b55a80302e53ac574d8a8ce9e8c39f6d
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-01-17 17:16:36 +0200

    thisCommit

--- exp1/middlewares/logger.js
+++
@@ -1,6 +0,0 @@
-function log (req,res,next){
-    console.log('logging the action .....')
-    next()
-  }
-
-module.exports = log
--- node-modules-js/pathModuleJs.js
+++
@@ -1,8 +0,0 @@
-const path = require('path')
-// module 
-// file 
-// node_modles
-
-let pathObj = path.parse(__filename)
-
-console.log(pathObj);
