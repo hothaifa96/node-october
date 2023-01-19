481b61940061c9c1e44e9e1c2d6257464f01e3bc
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-01-19 11:01:36 +0200

    message

--- m.py
+++ m.py
@@ -1,8 +1,13 @@
 import os
 import time
+from pathlib import Path
 # automate a add comit an push procces of an git repo with python code
-os.system('git add .')
-time.sleep(0.5)
-os.system(f'git commit -m message')
-time.sleep(3)
-os.system('git push')
+s=str(Path())
+s=s[:5]
+print(s)
+# os.chdir(str(Path())[:-5])
+# os.system('git add .')
+# time.sleep(0.5)
+# os.system(f'git commit -m message')
+# time.sleep(3)
+# os.system('git push')
